7bb05b168aa2e7e62b424287ce8690053e4bea2f
Om Jadav <omjadav04@gmail.com>
2023-04-09 21:27:03 +0530

    php edit

--- SchoolSITE/form-handler.php
+++ SchoolSITE/form-handler.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST[''];
 $subject=$_POST[''];
 $message=$_POST[''];
 
-$email_from='omjadav04@gmail.com';
+$email_from='https://om117.github.io/SchoolSITE/index.html';
 
 $email_subject='New Form Submission';
 
