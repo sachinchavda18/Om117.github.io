31f679ea70efd936b309810ae4979853d246d888
Om Jadav <omjadav04@gmail.com>
2023-04-09 20:26:03 +0530

    updated contactus

---
+++ SchoolSITE/form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name=$_POST[''];
+$visitor_email=$_POST[''];
+$subject=$_POST[''];
+$message=$_POST[''];
+
+$email_from='omjadav04@gmail.com';
+
+$email_subject='New Form Submission';
+
+$email_body= "User Name: $name.\n".
+                "User Email: $visitor_email.\n". 
+                "User Subject: $subject.\n". 
+                "User Message: $message.\n";
+
+$to = 'omjadav2002@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contactus.html")
+?>
